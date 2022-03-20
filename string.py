aa22895fd8936e9a16aa383cde761ff18cddbfd2
Lyov <levon.hambardzumyan@exio.tech>
2022-03-20 14:27:30 +0400

    String works

---
+++ string.py
@@ -0,0 +1,8 @@
+surname = "Hambardzumyan"
+print(surname)
+print(surname.upper())
+print(surname.lower())
+print(surname.replace("a", "f"))
+print(surname.find("m") )
+print(len(surname))
+print(surname[0:8:2])
